c4c33de2eca1ee16d2293ff6c20a49e542669af8
SachinDevelopment <53714757+SachinDevelopment@users.noreply.github.com>
2020-10-26 22:12:44 -0400

    sample requests

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require("express");
+var bodyParser = require('body-parser')
 const app = express();
 const port = 5000;
 
-app.get("/", (req, res) => {
+app.use(bodyParser.json());
+
+// get sample
+app.get("/api", async (req, res) => {
   res.send("Hello World!");
 });
+
+// post sample
+app.post("/parth", async (req, res) => {
+  console.log('printing out body', req.body);
+  res.sendStatus(200);
+});
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
