c9ae8087213e66e5119fdb54a04abbc9f1bb9ad1
SachinDevelopment <53714757+SachinDevelopment@users.noreply.github.com>
2020-10-26 22:52:16 -0400

    working call from reactapp

--- app.js
+++ app.js
@@ -2,12 +2,42 @@ const express = require("express");
 var bodyParser = require('body-parser')
 const app = express();
 const port = 5000;
+const cors = require('cors');
 
 app.use(bodyParser.json());
-
+app.use(cors());
 // get sample
-app.get("/api", async (req, res) => {
-  res.send("Hello World!");
+app.get("/api/colors", async (req, res) => {
+  res.send([
+    {
+      color: "red",
+      value: "#f00"
+    },
+    {
+      color: "green",
+      value: "#0f0"
+    },
+    {
+      color: "blue",
+      value: "#00f"
+    },
+    {
+      color: "cyan",
+      value: "#0ff"
+    },
+    {
+      color: "magenta",
+      value: "#f0f"
+    },
+    {
+      color: "yellow",
+      value: "#ff0"
+    },
+    {
+      color: "black",
+      value: "#000"
+    }
+  ]);
 });
 
 // post sample
